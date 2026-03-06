6b31abd57650078032797377275feec0136f4293
Hans Georg Schaathun <georg@schaathun.net>
2026-03-06 16:26:30 +0100

    new mode

--- batchtest.sh
+++ batchtest.sh
@@ -1,7 +1,8 @@
 
-python -m ChatRunner --config config/idun-simple.toml \
+python -m ChatRunner --config config/idun-key.toml \
                      --batch Example/exphil.toml \
 		     --outfile exphil2idun.toml \
+		     --mode new \
 		     --verbose \
 		     --count 2 --verbose 2> exphil2idun.err > exphil2idun.log &
 
