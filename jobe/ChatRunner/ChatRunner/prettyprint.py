f06eb9344ce2fa6de70c1e9ab9bc5fba218a8ec2
Hans Georg Schaathun <georg@schaathun.net>
2026-03-06 13:22:42 +0100

    more drafting for prettyprint

--- jobe/ChatRunner/ChatRunner/prettyprint.py
+++ jobe/ChatRunner/ChatRunner/prettyprint.py
@@ -9,6 +9,7 @@ if __name__ == "__main__":
         prog = 'ChatRunner.prettyprint',
         description = 'Pretty print AI feedback from TOML output',
         epilog = '')
+    parser.add_argument('outfile',help="Output file")
     parser.add_argument('file',help="Feedback file")
     args = parser.parse_args()
 
@@ -23,3 +24,17 @@ if __name__ == "__main__":
 
     qs = feedback["questions"]
     print( "Questions", type(qs), len(qs) )
+    result = [ "# ChatRunner test", "" ]
+    for qno, q in enumerate(qs):
+        result.append( f"## Question {qno}" )
+        result.append( "" )
+        result.append( "> " + q["question]" )
+        result.append( "" )
+        for ano, a in enumerate( q["answers"] ):
+            result.append( f"### Answer no. {qno}-{rno}" )
+            result.append( "" )
+            result.append( "> " + a["ans]" )
+            result.append( "" )
+    with open(args.outfile, "w") as f:
+        f.write("\n".join(result))
+
