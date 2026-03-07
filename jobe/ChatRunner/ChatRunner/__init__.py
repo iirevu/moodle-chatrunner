75aca5a56b68b45cf19805d1f071fcec9cd76a4e
Hans Georg Schaathun <georg@schaathun.net>
2026-03-07 21:29:48 +0100

    docstring

--- jobe/ChatRunner/ChatRunner/__init__.py
+++ jobe/ChatRunner/ChatRunner/__init__.py
@@ -1,7 +1,8 @@
 # (C) 2025: Hans Georg Schaathun <hasc@ntnu.no>
 
 """
-ChatRunner is not yet prepared as a package.
-The directory contains code to use chatrunner.py as module
-loaded from the working directory.
+ChatRunner manages quiz questions with automated grading by an LLM.
+
+It is intended for use from CodeRunner in Moodle, but can also be
+run in batch to test feedback quality of LLMs.
 """
