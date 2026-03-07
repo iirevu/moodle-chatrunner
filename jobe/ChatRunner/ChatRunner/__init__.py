dd0f59eabd053d126a8f30e5c870a2b7e20a5fe7
Hans Georg Schaathun <georg+professor@schaathun.net>
2026-03-07 23:03:29 +0100

    simplification - export queryAI from top-level package

--- jobe/ChatRunner/ChatRunner/__init__.py
+++ jobe/ChatRunner/ChatRunner/__init__.py
@@ -6,3 +6,5 @@ ChatRunner manages quiz questions with automated grading by an LLM.
 It is intended for use from CodeRunner in Moodle, but can also be
 run in batch to test feedback quality of LLMs.
 """
+
+from .query import queryAI
